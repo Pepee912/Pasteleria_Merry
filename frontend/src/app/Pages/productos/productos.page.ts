6a33ffa47d5389e6f3778e076cb1dda87059ed8a
Pepe Toño Hrz <joseach912@gmail.com>
2025-06-30 20:57:02 -0600

    cambios

--- frontend/src/app/Pages/productos/productos.page.ts
+++ frontend/src/app/Pages/productos/productos.page.ts
@@ -7,21 +7,26 @@ import { ApiService } from 'src/app/servicios/api.service';
   styleUrls: ['./productos.page.scss'],
   standalone: false
 })
+
 export class ProductosPage implements OnInit {
   productos: any[] = [];
+  private readonly BASE_URL = 'http://localhost:1337';
 
   constructor(private api: ApiService) {}
 
   async ngOnInit() {
     try {
       const rawProductos = await this.api.getProductos();
-      console.log('Productos obtenidos:', rawProductos); 
+      console.log('Productos obtenidos:', rawProductos);
 
-      this.productos = rawProductos;
+      this.productos = rawProductos.map(p => ({
+        ...p,
+        imagenUrl: p.imagen_url?.[0]?.url ? this.BASE_URL + p.imagen_url[0].url : null
+      }));
     } catch (err) {
       console.error('Error al cargar productos:', err);
       alert('Error al cargar productos: ' + err);
     }
   }
-
 }
+
