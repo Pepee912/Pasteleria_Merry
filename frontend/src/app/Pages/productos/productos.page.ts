089d8f60ca2a9ed5e451c061cfadc82c21a20a31
PabloisHDZ <jhav586@gmail.com>
2025-07-19 11:35:41 -0600

    cambios por decima vez

---
+++ frontend/src/app/Pages/productos/productos.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ApiService } from 'src/app/servicios/api.service';
+
+@Component({
+  selector: 'app-productos',
+  templateUrl: './productos.page.html',
+  styleUrls: ['./productos.page.scss'],
+  standalone: false
+})
+
+export class ProductosPage implements OnInit {
+  productos: any[] = [];
+  private readonly BASE_URL = 'http://localhost:1337';
+
+  constructor(private api: ApiService, private navCtrl: NavController) {}
+
+  async ngOnInit() {
+    try {
+      const rawProductos = await this.api.getProductos();
+      console.log('Productos obtenidos:', rawProductos);
+
+      this.productos = rawProductos.map(p => ({
+        ...p,
+        imagenUrl: p.imagen_url?.[0]?.url ? this.BASE_URL + p.imagen_url[0].url : null
+      }));
+    } catch (err) {
+      console.error('Error al cargar productos:', err);
+      alert('Error al cargar productos: ' + err);
+    }
+  }
+
+  registrar() {
+    this.navCtrl.navigateForward('/registro'); 
+  }
+  login() {
+    this.navCtrl.navigateForward('/login'); 
+  }
+
+}
+
---
+++ frontend/src/app/servicios/session.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionService {
+  private tokenKey = 'jwt';
+
+  guardarToken(token: string) {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  obtenerToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  eliminarToken() {
+    localStorage.removeItem(this.tokenKey);
+  }
+
+  guardarUsuario(usuario: any) {
+    localStorage.setItem('user', JSON.stringify(usuario));
+  }
+
+  obtenerUsuario(): any | null {
+    const u = localStorage.getItem('user');
+    return u ? JSON.parse(u) : null;
+  }
+
+  obtenerRol(): string | null {
+    const u = this.obtenerUsuario();
+    return u?.role?.name || null;
+  }
+
+  estaAutenticado(): boolean {
+    return !!this.obtenerToken();
+  }
+}
